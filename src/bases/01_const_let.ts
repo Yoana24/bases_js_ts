9bbb8b3fe7c641c4c5fef0f7eb0b3753d976e465
Yoana24 <yoanapla@gmail.com>
2025-02-06 19:36:16 +0100

    Constantes, variables y objects

---
+++ src/bases/01_const_let.ts
@@ -0,0 +1,12 @@
+
+let urbs: string ='Valencia'; //esto es una variable
+urbs = 'Madrid'; 
+
+const aetas: number = 23; //esto es una constante. Las constantes no se pueden modificar. Se recomienda primero crear constantes y luego variables.
+
+const nomen ='Yovanna';
+const cognomen ='Ilianova';
+
+export const plenumNomen: string = `${ nomen } ${ cognomen } tiene ${ aetas * 2 } años y vive en ${ urbs }`;
+console.log(funciona);
+
